cef89524c0ccd258c82bafcbbc924996ae518a58
Dmitrii Ivanov <itdriver@yahoo.com>
2023-03-02 18:54:04 +0100

    menu adaptive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 let burger = document.querySelector('.burger');
+let close = document.querySelector('.close');
+let menuAdaptive = document.querySelector('.menu-adaptive');
 
 burger.addEventListener('click', () => {
+    menuAdaptive.classList.remove('hide');
+})
 
+close.addEventListener('click', () => {
+    menuAdaptive.classList.add('hide');
 })
