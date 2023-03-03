fa13d86944ad2f968bc8f591e19ef6b1cbaf086b
Dmitrii Ivanov <itdriver@yahoo.com>
2023-03-03 21:40:38 +0100

    hero ready

--- js/main.js
+++ js/main.js
@@ -2,10 +2,15 @@ let burger = document.querySelector('.burger');
 let close = document.querySelector('.close');
 let menuAdaptive = document.querySelector('.menu-adaptive');
 
+
+
 burger.addEventListener('click', () => {
     menuAdaptive.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
 })
 
 close.addEventListener('click', () => {
     menuAdaptive.classList.add('hide');
+    document.body.style.overflow = 'auto';
+
 })
